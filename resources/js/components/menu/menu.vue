d70f3cf9324942fdedce283ef155f9ea7d46778d
amin <garogigi@gmail.com>
2022-03-06 01:23:01 +0300

    cd

---
+++ resources/js/components/menu/menu.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="menu">
+        <div class="menu-top">
+            <h1>امين عمر</h1>
+            <hr>
+            <p>+1000000000 | email@email.com</p>
+        </div>
+        <div class="menu-list">
+            <div>
+                <p>اطبائي</p>
+            </div>
+            <div>
+                <p>مقابلاتي</p>
+            </div>
+            <div>
+                <p>تسجيل خروج</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+
+
+        return {}
+    }
+}
+</script>
+
+<style>
+    .menu-top {
+        color: #fff;
+        background-color: #2288dd;
+    }
+
+    .menu-list{
+        background-color: #d8d8d7;
+    }
+</style>
